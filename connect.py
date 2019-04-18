3a5719530d8e902f9baa2eb7986f0bf348f97474
Novia-Git <43981539+Novia-Git@users.noreply.github.com>
2019-04-18 18:01:12 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,22 +1,22 @@
 import pyodbc
 
-#define sql connection
+#Define sql connection
 conn_str = "DRIVER={ODBC Driver 13 for SQL Server};SERVER=your server ip;DATABASE=your database name;UID=username;PWD=password"
 conn = pyodbc.connect(conn_str)
 cur = ariconn.cursor()
 
-#execute sql
+#Execute sql
 table_name = 'test'
 sql = '''SELECT * FROM '''+ table_name 
 cur.execute(sql)
 row = cur.fetchall()
 
-#print data
+#Print data
 for i in row:
     print(i)
 
 cur.commit()     
 
-#close connection
+#Close connection
 cur.close()
 conn.close()
